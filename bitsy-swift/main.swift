c01a3704cd9d2189bf6d01be019700851319b470
Ben DiFrancesco <ben.difrancesco@gmail.com>
2016-07-01 16:24:35 -0400

    Initial Commit

---
+++ bitsy-swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  bitsy-swift
+//
+//  Created by Ben DiFrancesco on 7/1/16.
+//  Copyright © 2016 ScopeLift. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
