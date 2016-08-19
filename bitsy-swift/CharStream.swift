1918e8745a44439d952c10be1ee92b050cd297a1
Ben DiFrancesco <ben.difrancesco@gmail.com>
2016-08-19 14:28:35 -0400

    Adds comment docs for CharStream

--- bitsy-swift/CharStream.swift
+++ bitsy-swift/CharStream.swift
@@ -1,22 +1,40 @@
 import Foundation
 
+/**
+ * Convenient interface for advancing one `Character` at a time
+ * over a String
+ */
 struct CharStream {
     private let string: String
     private var index: String.CharacterView.Index
 
+    /**
+     * Initialize a character stream
+     *
+     * - parameter string: String to walk over
+     */
     init(string: String) {
         self.string = string
         self.index = self.string.startIndex
     }
 
+    /**
+     *  Peek at the current character
+     */
     var current: Character {
         return string[index]
     }
 
+    /**
+     *  Are there additional characters beyond `current`
+     */
     var hasMore: Bool {
         return index.successor() < string.endIndex
     }
 
+    /**
+     *  Move to `current` to the next Character if `hasMore`
+     */
     mutating func advance() {
         guard hasMore else { return }
         index = index.successor()
