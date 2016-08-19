e4f82aeaf76a0d6aaf5bec2645c54c33ff3cc597
Ben DiFrancesco <ben.difrancesco@gmail.com>
2016-08-19 14:35:30 -0400

    Adds doc comments for the TokenType enumeration

--- bitsy-swift/TokenType.swift
+++ bitsy-swift/TokenType.swift
@@ -1,18 +1,32 @@
 import Foundation
 
+/**
+ *  Enumaration of all kinds of Tokens available
+ *  For Parens, Operators, and Keywords, each case is backed
+ *  by a rawValue of the String representing that symbol in Bitsy
+ */
 enum TokenType: String {
+    // MARK: Seperators
     case whitespace
     case variable
+
+    // MARK: Identifiers
     case integer
     case comment
+
+    // MARK: Parens
     case leftParen  = "("
     case rightParen = ")"
+
+    // MARK: Operators
     case plus       = "+"
     case minus      = "-"
     case multiply   = "*"
     case divide     = "/"
     case modulus    = "%"
     case assignment = "="
+
+    // MARK: Keywords
     case begin      = "BEGIN"
     case end        = "END"
     case ifP        = "IFP"
@@ -23,6 +37,11 @@ enum TokenType: String {
     case breakKey   = "BREAK"
     case print      = "PRINT"
     case read       = "READ"
+}
 
+extension TokenType {
+    /**
+     * All `TokenType`s which are considered Bitsy operators
+     */
     static var operators: [TokenType] = [.plus, .minus, .multiply, .divide, .modulus, .assignment]
 }
